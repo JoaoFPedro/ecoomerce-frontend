08aecea9f60d7fc93403e585a626cb8dbd1e33ce
Joao Pedro <joao.fpsilva98@gmail.com>
2024-03-18 19:45:14 -0300

    core: set up eslint and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
