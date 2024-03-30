6d91ab885c7e0b583cef6f84d162adcd7044987a
Joao Pedro <joao.fpsilva98@gmail.com>
2024-03-30 15:26:03 -0300

    feat: Adding Loging button

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,4 +1,5 @@
 import { BsGoogle } from 'react-icons/bs'
+import { FiLogIn } from 'react-icons/fi'
 import CustomButton from '../../components/custom-button/custom-button.component'
 import Header from '../../components/header/header-component'
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from './login.styles'
@@ -15,6 +16,8 @@ const LoginPage = () => {
                 <CustomButton startIcon={<BsGoogle size={18}/>}>Entrar com o google</CustomButton>
 
                 <LoginSubtitle>ou entre com o seu email</LoginSubtitle>
+
+                <CustomButton startIcon={<FiLogIn size={18}/>}>Log In</CustomButton>
 
                 <LoginInputContainer>{/* Email Input */}</LoginInputContainer>
                 <LoginInputContainer>{/* Passoward Input */}</LoginInputContainer>
