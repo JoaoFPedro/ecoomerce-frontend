9cd9be8e7be656149ba738015b09447657085e0c
Joao Pedro <joao.fpsilva98@gmail.com>
2024-03-31 00:25:36 -0300

    feat: adding react hook form

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -10,8 +10,23 @@ import {
   LoginSubtitle
 } from './login.styles'
 import CustomInput from '../../components/custom-input/custom-input.component'
+import { useForm } from 'react-hook-form'
 
 const LoginPage = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm()
+
+  const handleSubmitPress = (data: any) => {
+    console.log({ data })
+  }
+
+
+  console.log({ errors })
+
+  
   return (
     <>
       <Header />
@@ -27,14 +42,27 @@ const LoginPage = () => {
 
           <LoginInputContainer>
             <p>Email</p>
-            <CustomInput placeholder='Digite seu e-mail' />
+            <CustomInput
+            hasError={!!errors?.email}
+              placeholder='Digite seu e-mail'
+              {...register('email', { required: true })}
+            />
           </LoginInputContainer>
           <LoginInputContainer>
             <p>Senha</p>
-            <CustomInput placeholder='Digite sua senha' />
+            <CustomInput
+             hasError={!!errors?.passowrd}
+              placeholder='Digite sua senha'
+              {...register('passowrd', { required: true })}
+            />
           </LoginInputContainer>
 
-          <CustomButton startIcon={<FiLogIn size={18} />}>Log In</CustomButton>
+          <CustomButton
+            startIcon={<FiLogIn size={18} />}
+            onClick={() => handleSubmit(handleSubmitPress)()}
+          >
+            Log In
+          </CustomButton>
         </LoginContent>
       </LoginContainer>
     </>
