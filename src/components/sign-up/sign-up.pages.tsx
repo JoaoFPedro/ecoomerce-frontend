2db3ddc697ba20d2e71c76b28e3c30af8b08ae03
Joao Pedro <joao.fpsilva98@gmail.com>
2024-04-01 20:15:28 -0300

    feat: Adding container and some logic at the component

--- src/components/sign-up/sign-up.pages.tsx
+++ src/components/sign-up/sign-up.pages.tsx
@@ -1,7 +1,76 @@
+import { useForm } from 'react-hook-form'
+import CustomInput from '../custom-input/custom-input.component'
+import Header from '../header/header-component'
+import {
+  SignUpContainer,
+  SignUpContent,
+  SignUpHeadline,
+  SignUpInputContainer
+} from './sign-up.page.styles'
+import CustomButton from '../custom-button/custom-button.component'
+import { FiLogIn } from 'react-icons/fi'
+import validator from 'validator'
+
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const handleSubmitPress = (data: any) => {
+    console.log({ data })
+  }
   return (
     <>
-      <h1>Sign up Page</h1>
+      <Header />
+      <SignUpContainer>
+        <SignUpContent>
+          <SignUpHeadline>Criar sua conta</SignUpHeadline>
+          <SignUpInputContainer>
+            <p>Nome</p>
+            <CustomInput
+              {...register('name', {
+                required: true,
+                validate: (value) => {
+                  return validator.isEmail(value)
+                }
+              })}
+              hasError={!!errors?.name}
+              placeholder='Digite seu nome '
+            ></CustomInput>
+          </SignUpInputContainer>
+          <SignUpInputContainer>
+            <p>Sobrenome</p>
+            <CustomInput placeholder='Digite seu sobrenome '></CustomInput>
+          </SignUpInputContainer>
+          <SignUpInputContainer>
+            <p>Email</p>
+            <CustomInput placeholder='Digite seu email '></CustomInput>
+          </SignUpInputContainer>
+          <SignUpInputContainer>
+            <p>Senha</p>
+            <CustomInput
+              type='password'
+              placeholder='Digite sua senha '
+              {...register}
+            ></CustomInput>
+          </SignUpInputContainer>
+          <SignUpInputContainer>
+            <p>Confirmar Senha</p>
+            <CustomInput
+              type='password'
+              placeholder='Digite sua senha novamente'
+            ></CustomInput>
+          </SignUpInputContainer>
+          <CustomButton
+            startIcon={<FiLogIn size={18} />}
+            onClick={() => handleSubmit(handleSubmitPress)()}
+          >
+            Criar Conta
+          </CustomButton>
+        </SignUpContent>
+      </SignUpContainer>
     </>
   )
 }
