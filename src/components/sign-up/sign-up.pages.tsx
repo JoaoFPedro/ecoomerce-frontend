074862d5fc6de388d63ac288576db9cdfc39220e
Joao Pedro <joao.fpsilva98@gmail.com>
2024-04-02 19:39:23 -0300

    feat: Adding validation form for the inputs

--- src/components/sign-up/sign-up.pages.tsx
+++ src/components/sign-up/sign-up.pages.tsx
@@ -9,9 +9,15 @@ import {
 } from './sign-up.page.styles'
 import CustomButton from '../custom-button/custom-button.component'
 import { FiLogIn } from 'react-icons/fi'
+import { isEmail } from 'validator'
+import InputErrorMessage from '../input-error-message/input-error-message.component'
 
-interface SignUpForm{
-
+interface SignUpForm {
+  name: string
+  lastName: string
+  email: string
+  password: string
+  passwordConfirmation: string
 }
 
 const SignUpPage = () => {
@@ -19,7 +25,7 @@ const SignUpPage = () => {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
+  } = useForm<SignUpForm>()
 
   const handleSubmitPress = (data: any) => {
     console.log({ data })
@@ -38,36 +44,62 @@ const SignUpPage = () => {
               })}
               hasError={!!errors?.name}
               placeholder='Digite seu nome '
-            ></CustomInput>
+            /> {errors?.name?.type === 'required' && (
+              <InputErrorMessage>O nome é obrigatório</InputErrorMessage>
+            )}
           </SignUpInputContainer>
           <SignUpInputContainer>
             <p>Sobrenome</p>
             <CustomInput
-              {...register('lastname', {
+              {...register('lastName', {
                 required: true
               })}
               placeholder='Digite seu sobrenome '
-              hasError={!!errors?.lastname}
-            ></CustomInput>
+              hasError={!!errors?.lastName}
+            />
+            {errors?.lastName?.type === 'required' && (
+              <InputErrorMessage>O Sobrenome é obrigatório.</InputErrorMessage>
+            )}
           </SignUpInputContainer>
           <SignUpInputContainer>
             <p>Email</p>
-            <CustomInput placeholder='Digite seu email '></CustomInput>
+            <CustomInput
+              {...register('email', {
+                required: true,
+                validate: (value) => {
+                  return isEmail(value)
+                }
+              })}
+              placeholder='Digite seu email '
+              hasError={!!errors?.email}
+            />
+            {errors?.email?.type === 'required' && (
+              <InputErrorMessage>O email é obrigatório.</InputErrorMessage>
+            )}
           </SignUpInputContainer>
           <SignUpInputContainer>
             <p>Senha</p>
             <CustomInput
               type='password'
               placeholder='Digite sua senha '
-              {...register}
-            ></CustomInput>
+              {...register('password', {
+                required: true
+              })}
+              hasError={!!errors?.password}
+            /> {errors?.password?.type === 'required' && (
+              <InputErrorMessage> A senha é obrigatória.</InputErrorMessage>
+            )}
           </SignUpInputContainer>
           <SignUpInputContainer>
             <p>Confirmar Senha</p>
             <CustomInput
               type='password'
               placeholder='Digite sua senha novamente'
-            ></CustomInput>
+              {...register('passwordConfirmation', { required: true })}
+              hasError={!!errors?.passwordConfirmation}
+            /> {errors?.passwordConfirmation?.type === 'required' && (
+              <InputErrorMessage>Confirme sua senha.</InputErrorMessage>
+            )}
           </SignUpInputContainer>
           <CustomButton
             startIcon={<FiLogIn size={18} />}
