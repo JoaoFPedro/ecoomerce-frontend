736aa2ceea690c7d6852dbd395144d873292377d
Joao Pedro <joao.fpsilva98@gmail.com>
2024-05-20 21:09:42 -0300

    style: add responsivity to category overview component

--- src/components/categories-overview/categories.overview.styles.ts
+++ src/components/categories-overview/categories.overview.styles.ts
@@ -2,4 +2,9 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   padding: 0px 40px 20px 40px;
+
+  
+  @media(max-width: 768px) {
+    padding: 0px 40px 20px 70px;
+  }
 `
