20508a3251e52caef2b896c4266b0ad912c57ddf
Joao Pedro <joao.fpsilva98@gmail.com>
2024-05-25 10:32:54 -0300

    test: ensure custom input change values on user type

--- src/components/custom-input/custom-input.spec.tsx
+++ src/components/custom-input/custom-input.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react'
 import CustomInput from './custom-input.component'
 import Colors from '../../themes/theme.colors'
+import userEvent from '@testing-library/user-event'
 
 describe('Custom Input', () => {
   it('should render with error if hasError is true', () => {
@@ -18,5 +19,16 @@ describe('Custom Input', () => {
     )
     const input = getByPlaceholderText('Lorem Ipsum')
     expect(input).toHaveStyle({ border: 'none' })
+  })
+
+  it('should change value when user types', () => {
+    const { getByPlaceholderText, getByDisplayValue } = render(
+      <CustomInput placeholder='Lorem Ipsum' haserror={false} />
+    )
+    const input = getByPlaceholderText('Lorem Ipsum')
+    
+
+    userEvent.type(input, 'Dolor Sit')
+    getByDisplayValue('Dolor Sit')
   })
 })
